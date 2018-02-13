150ea9b0c0fc8f0081711528872e0e71abae675c
Jeff Whelpley <jeff@gethuman.com>
2018-02-13 08:19:35 -0800

    Test GraphQL app in place

---
+++ ua-backend/src/server.ts
@@ -0,0 +1,12 @@
+import * as express from 'express';
+import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
+import * as bodyParser from 'body-parser';
+import schema from './schemas';
+
+const GRAPHQL_PORT = 3000;
+const graphQLServer = express();
+
+graphQLServer.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
+graphQLServer.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
+
+graphQLServer.listen(GRAPHQL_PORT, () => console.log(`GraphiQL is now running on http://localhost:${GRAPHQL_PORT}/graphiql`));
